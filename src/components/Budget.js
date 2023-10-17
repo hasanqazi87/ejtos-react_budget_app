7bd86346ffbdcde219702c45d05faa7180dc587e
Hasan Qazi <hasanqazi87@gmail.com>
2023-10-17 04:23:51 -0400

    Update budget React app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,8 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const {expenses} = useContext(AppContext);
+    const {budget, expenses, currency} = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
@@ -22,7 +21,7 @@ const Budget = () => {
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: $</span>
+<span>Budget: {currency}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {AppContext} from '../context/AppContext';
 
 const Currency = () => {
+    const {dispatch} = useContext(AppContext);
+    const changeCurrency = (val) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: val,
+        });
+    };
     return (
         <div className='alert alert-primary'>
         <span>Currency: </span>
-        <select style={{backgroundColor: '#badbbc'}}>
-            <option value='usd'>$ Dollar</option>
-            <option value='gbp'>£ Pound</option>
-            <option value='eur'>€ Euro</option>
-            <option value='ind'>₹ Ruppee</option>
+        <select style={{backgroundColor: '#badbbc'}} id='currency' onChange={evt=>changeCurrency(evt.target.value)}>
+            <option value='$'>$ Dollar</option>
+            <option value='£'>£ Pound</option>
+            <option value='€'>€ Euro</option>
+            <option value='₹'>₹ Ruppee</option>
         </select>
         </div>
     );
